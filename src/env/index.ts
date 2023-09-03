a7b3d498e27a4007cf539800df3ad5929e80f2f5
Saymon Damásio <saymon.damasio95@gmail.com>
2023-09-02 22:08:22 -0300

    chore: init project

---
+++ src/env/index.ts
@@ -0,0 +1,23 @@
+import { config } from 'dotenv'
+
+import { z } from 'zod'
+
+if (process.env.NODE_ENV) {
+  config({ path: `.env.${process.env.NODE_ENV}` })
+} else {
+  config()
+}
+
+const envSchema = z.object({
+  PORT: z.number().default(3333),
+  NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('Invalid variables', _env.error.format())
+  throw new Error('Invalid environment variables')
+}
+
+export const env = _env.data
