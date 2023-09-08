38545627c160a4b5dc13725a3126b9b497a2f5d6
Saymon Damásio <saymon.damasio95@gmail.com>
2023-09-07 23:19:52 -0300

    feat: global handler erros

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,3 +1,4 @@
+import { error } from 'console'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { PrismaUsersRepository } from '~/repositories/prisma/prisma-users-repository'
@@ -25,7 +26,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       })
     }
 
-    return reply.status(500).send()
+    throw error
   }
 
   return reply.status(201).send()
