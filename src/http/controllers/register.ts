adb021753614589996c731527656b3f8f6a177cd
Saymon Damásio <saymon.damasio95@gmail.com>
2023-09-07 23:08:28 -0300

    refactor: use cases errors

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { PrismaUsersRepository } from '~/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '~/use-cases/errors/user-already-exists-error'
 import { RegisterUseCase } from '~/use-cases/register'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
@@ -18,7 +19,13 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     await registerUseCase.execute({ email, name, password })
   } catch (error) {
-    return reply.status(409).send()
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({
+        error: error.message,
+      })
+    }
+
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
