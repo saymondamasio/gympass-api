82dc6929fcaf83d41739dd29f745305735e784c5
Saymon Damásio <saymon.damasio95@gmail.com>
2023-09-17 12:22:07 -0300

    test: e2e register

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,1 +1,22 @@
-test('opa', () => {})
+import request from 'supertest'
+import { app } from '~/app'
+
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toBe(201)
+  })
+})
